edfeaf9d477679542e6c2e30d8267b804182b391
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-04-08 16:48:06 +0200

    Remove HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,14 @@
 const path = require("path");
-// eslint-disable-next-line import/no-extraneous-dependencies
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Battleship",
-    }),
-  ],
   devtool: "inline-source-map",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
   module: {
     rules: [
