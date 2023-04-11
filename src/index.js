7050cba699cbded3108ab8e20359de92de41b9bf
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-04-11 16:08:29 +0200

    Change website icon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./Styles/style.css";
 import startGame from "./Scripts/game";
+import logo from "./Images/ship.png";
+
+// Change website icon
+const icon = document.createElement("link");
+icon.rel = "icon";
+icon.href = logo;
+document.getElementsByTagName("head")[0].appendChild(icon);
+
 
 startGame();
